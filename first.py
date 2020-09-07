9b5b50536c171c400359fee96acf0c3c8ef872ce
baduraje@isu.edu <baduraje@isu.edu>
2020-09-07 00:30:27 -0600

    added dummy data and x and y labels for bar chart

--- first.py
+++ first.py
@@ -2,3 +2,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as pl
 
+Year = [2001,2002,2003,2004,2005,2006,2007]
+
+Revenue = [10,15,9,5,20,30,40]
+
+# bar argument plots data. 
+
+pl.bar(x=np.arange(1,21),height=['Revenue'])
+
+pl.show()
